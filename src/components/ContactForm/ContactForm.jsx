cc07c2864e60af807a5f3a01c39967209fdd2022
VitaliyBolgarov <125477680+VitaliyBolgarov@users.noreply.github.com>
2023-09-20 19:23:28 +0300

    1.3

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -10,23 +10,6 @@ import {
   FieldInput,
 } from './ContactForm.styled';
 
-// const SignupSchema = Yup.object().shape({
-//   name: Yup.string()
-//     .trim()
-//     .matches(
-//       /^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$/,
-//       'Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d`Artagnan'
-//     )
-//     .required(),
-//   number: Yup.string()
-//     .trim()
-//     .matches(
-//       /\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}/,
-//       'Phone number must be digits and can contain spaces, dashes, parentheses and can start with +'
-//     )
-//     .required(),
-// });
-
 export const ContactForm = ({ onAddContact }) => {
   const {
     register,
